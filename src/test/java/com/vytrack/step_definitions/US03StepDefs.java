dbab2b733ef34d332cae0af2e00b0619c5ca6f70
Safiye_Mertcan <safiyemertcan@hotmail.com>
2023-07-19 23:08:39 +0200

    click issue solved on US03StepDefs

--- src/test/java/com/vytrack/step_definitions/US03StepDefs.java
+++ src/test/java/com/vytrack/step_definitions/US03StepDefs.java
@@ -2,11 +2,11 @@ package com.vytrack.step_definitions;
 
 import com.vytrack.pages.PinbarPage;
 import com.vytrack.utilities.BrowserUtilities;
-import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class US03StepDefs {
 
@@ -18,37 +18,31 @@ public class US03StepDefs {
     public void user_is_on_the_dashboard_page() {
 
         BrowserUtilities.waitForTitleContains("Dashboard");
-        String actualTitle = Driver.getDriver().getTitle();
-        String expectedTitle = "Dashboard";
-        Assert.assertEquals("Title verification failed", expectedTitle, actualTitle);
+        BrowserUtilities.verifyTitle("Dashboard");
 
     }
-
 
     @When("user clicks Learn how to use this space link")
     public void user_clicks_link() {
 
-        pinbarPage.pinBarLearnLink.click();
+        pinbarPage.pinBarLearnLink.sendKeys(Keys.ENTER);
     }
 
 
     @Then("user should see {string}")
     public void user_should_see(String expectedHeader) {
 
-        // Wait for the "How To Use Pinbar" heading to be visible
-        BrowserUtilities.waitForVisibility(pinbarPage.header,3);
+        BrowserUtilities.waitForVisibility(pinbarPage.header, 3);
 
-        // Verify the heading text
         String actualHeader = pinbarPage.header.getText();
-        Assert.assertEquals(expectedHeader,actualHeader );
+        Assert.assertEquals(expectedHeader, actualHeader);
 
     }
 
-
     @Then("user sees {string}")
-    public void user_should_see_use_pin_icon_on_the_right_top_corner_of_page_to_create_fast_access_link_in_the_pinbar(String expectedText ) {
+    public void user_should_see_use_pin_icon_on_the_right_top_corner_of_page_to_create_fast_access_link_in_the_pinbar(String expectedText) {
 
-        BrowserUtilities.waitForVisibility(pinbarPage.pinBarText,2);
+        BrowserUtilities.waitForVisibility(pinbarPage.pinBarText, 2);
         String actualText = pinbarPage.pinBarText.getText();
 
         Assert.assertEquals(expectedText, actualText);
