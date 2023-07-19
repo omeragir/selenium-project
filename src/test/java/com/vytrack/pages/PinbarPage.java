9a101f9b451208452a3ef4f5f11629f88b4269c3
Safiye_Mertcan <safiyemertcan@hotmail.com>
2023-07-19 13:53:49 +0200

    US03 PinbarPage feature StepDefs

---
+++ src/test/java/com/vytrack/pages/PinbarPage.java
@@ -0,0 +1,29 @@
+package com.vytrack.pages;
+
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PinbarPage {
+
+    public PinbarPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//a[text()='Learn how to use this space']")
+    public WebElement pinBarLearnLink;
+
+    @FindBy(xpath = "//div[@class='clearfix']/h3")
+    public WebElement header;
+
+    @FindBy(xpath = "//div[@class='clearfix']//p[1]")
+    public WebElement pinBarText;
+
+    @FindBy(xpath = "//img[@src='/bundles/oronavigation/images/pinbar-location.jpg']")
+    public WebElement pinBarImage;
+
+
+
+}
