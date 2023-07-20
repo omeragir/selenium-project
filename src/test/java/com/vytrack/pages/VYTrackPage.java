4241672cd48aa378f83649ca62d50d4717c99876
mamihami <hamide.pehlivan@hotmail.com>
2023-07-20 10:27:12 +0200

    added new FindBy annotations

--- src/test/java/com/vytrack/pages/VYTrackPage.java
+++ src/test/java/com/vytrack/pages/VYTrackPage.java
@@ -1,6 +1,8 @@
 package com.vytrack.pages;
 
 import com.vytrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.awt.print.PageFormat;
@@ -10,4 +12,11 @@ public class VYTrackPage {
     public VYTrackPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "(//button[@class='close'])[1]")
+    public WebElement applicationAlertMessage;
+
+    @FindBy(xpath = "//div[@class='message']")
+    public WebElement permissionAlertMessage;
+
 }
