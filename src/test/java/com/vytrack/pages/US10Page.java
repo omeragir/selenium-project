a47d5dc7607f90c66fc532e7514c1cf4c2be8065
mkysll <mervefeyzakaraca@gmail.com>
2023-07-21 14:09:59 +0200

    US10 edit

---
+++ src/test/java/com/vytrack/pages/US10Page.java
@@ -0,0 +1,38 @@
+package com.vytrack.pages;
+
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class US10Page {
+
+    public US10Page(){
+        PageFactory.initElements(Driver.getDriver(),  this);
+    }
+
+    @FindBy(className = "loader-mask")
+    public WebElement loadingBar;
+
+
+    @FindBy(xpath = "(//button[@class='close'])[1]")
+    public WebElement alert1;
+
+
+    @FindBy(xpath = "//span[normalize-space()='Activities']")
+    public WebElement activities;
+
+    @FindBy(xpath = "//span[normalize-space()='Calendar Events']")
+    public  WebElement calendarEvent;
+
+    @FindBy(xpath = "//*[@id=\"container\"]/div[2]/div/div/div[2]/div/a")
+    public WebElement createCalendarEvent;
+
+
+
+
+
+
+
+
+}
