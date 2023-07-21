5c0f907ec126ca36c411b24c8b6a2794f632063d
sevilayunlu <sevilayunlu@icloud.com>
2023-07-21 13:10:56 +0200

    added Us08 jira updates

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
             features = "src/test/resources/features",
             glue = "com/vytrack/step_definitions",
             dryRun = false,// [true] it checks if all snippets are implemented.
-            tags = "", // @admin or @student or @regression
+            tags = "@B29G1-287", // @admin or @student or @regression
             publish = true
     )
     public class CukesRunner {
